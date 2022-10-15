8ea34b47800a0153f370e2dad608e0d47c9f2789
Dennis Maier <maier.dennis@outlook.de>
2022-10-15 13:25:18 +0200

    neu

--- script.js
+++ script.js
@@ -84,3 +84,9 @@ function showQuestions() {
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
+
+function answer(selection){
+    if(selection == questions[currentQuestion]['righ_answer']){
+        alert("richtig");
+    }
+}

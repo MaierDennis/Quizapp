5fc761cc1fc898e4ee1f8dcd6f124baed29c4065
Dennis Maier <maier.dennis@outlook.de>
2022-10-15 12:31:51 +0200

    new

--- script.js
+++ script.js
@@ -65,3 +65,22 @@ let questions = [
 }
 
 ];
+
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('numberOfQuestions').innerHTML = questions.length;
+
+    showQuestions();
+}
+
+function showQuestions() {
+    let question = questions[currentQuestion];
+    
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}

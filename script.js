2cda8c5c4f304bd7d1bbce2ca7efbbad6235e328
Dennis Maier <maier.dennis@outlook.de>
2022-10-17 09:43:58 +0200

    neu

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ let questions = [
     "righ_answer" : 3
 },
 {
-    "question" : "Wer hat HTML erfunden?",
-    "answer_1" : "Robbie Williams",
-    "answer_2" : "Lady Gaga",
-    "answer_3" : "Tim Berners-Lee",
-    "answer_4" : "Justin Bieber", 
+    "question" : "Wer hats erfunden?",
+    "answer_1" : "Deutschland",
+    "answer_2" : "Die Schweiz",
+    "answer_3" : "Österreich",
+    "answer_4" : "Singapur", 
     "righ_answer" : 3
 },
 {
@@ -77,6 +77,8 @@ function init() {
 
 function showQuestions() {
     let question = questions[currentQuestion];
+
+    document.getElementById('numberCurrentQuestion').innerHTML = currentQuestion + 1;
     
     document.getElementById('questionText').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
@@ -87,17 +89,35 @@ function showQuestions() {
 
 function answer(selection){
     let question = questions[currentQuestion];
-    console.log('Selected answer is', selection);
     let selectedQuestionNumber = selection.slice(-1);
-    console.log('selectedQuestionNumber is', selectedQuestionNumber);
-    console.log('Current question is', question['right_answer']);
+    let idOfRightAnswer = `answer_${question['righ_answer']}`;
 
     if(selectedQuestionNumber == question['righ_answer']){
-        
         document.getElementById(selection).classList.add('bg-success');
     }
-    else {
-        
+    else {  
         document.getElementById(selection).classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).classList.add('bg-success');
     }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestions();
+}
+
+function resetAnswerButtons(){
+    
+        document.getElementById('answer_1').classList.remove('bg-danger');
+        document.getElementById('answer_1').classList.remove('bg-success');
+        document.getElementById('answer_2').classList.remove('bg-danger');
+        document.getElementById('answer_2').classList.remove('bg-success');
+        document.getElementById('answer_3').classList.remove('bg-danger');
+        document.getElementById('answer_3').classList.remove('bg-success');
+        document.getElementById('answer_4').classList.remove('bg-danger');
+        document.getElementById('answer_4').classList.remove('bg-success');
+    
 }

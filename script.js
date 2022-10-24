69d44ddf92f6e928c7bbd9ae79b13a36a94ebf27
Dennis Maier <maier.dennis@outlook.de>
2022-10-24 12:12:56 +0200

    fertig

--- script.js
+++ script.js
@@ -8,60 +8,60 @@ let questions = [
         "right_answer": 3
     },
     {
-        "question": "Wer hat Ricola erfunden?",
-        "answer_1": "Deutschland",
-        "answer_2": "Die Schweiz",
-        "answer_3": "Österreich",
-        "answer_4": "Singapur",
+        "question": "Which HTML element describes the largest heading?",
+        "answer_1": "heading",
+        "answer_2": "head",
+        "answer_3": "h1",
+        "answer_4": "h6",
+        "right_answer": 3
+    },
+    {
+        "question": "Which input type defines a slider control?",
+        "answer_1": "controls",
+        "answer_2": "range",
+        "answer_3": "slider",
+        "answer_4": "search",
         "right_answer": 2
     },
     {
-        "question": "Liegt Tengen vor, hinter oder neben dem Berg?",
-        "answer_1": "Vor",
-        "answer_2": "Hinter",
-        "answer_3": "Rechts daneben",
-        "answer_4": "Links daneben",
+        "question": "How can you make a numbered list?",
+        "answer_1": "ul",
+        "answer_2": "ol",
+        "answer_3": "list",
+        "answer_4": "dl",
+        "right_answer": 2
+    },
+    {
+        "question": "How can you make a bulleted list?",
+        "answer_1": "ul",
+        "answer_2": "ol",
+        "answer_3": "list",
+        "answer_4": "dl",
         "right_answer": 1
     },
     {
-        "question": "Welche Automarke übertrifft alle anderen?",
-        "answer_1": "BMW",
-        "answer_2": "VW",
-        "answer_3": "Mercedes",
-        "answer_4": "Audi",
-        "right_answer": 4
-    },
-    {
-        "question": "Wie ist das Wetter am Mittwoch?",
-        "answer_1": "Klar",
-        "answer_2": "Bewölkt",
-        "answer_3": "Regen",
-        "answer_4": "Woher soll ich das wissen?",
-        "right_answer": 4
-    },
-    {
-        "question": "Wer schimpft sich Idiot?",
-        "answer_1": "Andre",
-        "answer_2": "Jonas",
-        "answer_3": "Frieder",
-        "answer_4": "Alle davon",
-        "right_answer": 4
-    },
-    {
-        "question": "Wo liegt Miramundo",
-        "answer_1": "Iran",
-        "answer_2": "Guatemala",
-        "answer_3": "Kasachstan",
-        "answer_4": "Neuseeland",
+        "question": "Which HTML element is used to specify a header for a document or section?",
+        "answer_1": "section",
+        "answer_2": "header",
+        "answer_3": "top",
+        "answer_4": "head",
         "right_answer": 2
     },
     {
-        "question": "wie viel Ar sind ein Hektar?",
-        "answer_1": "10",
-        "answer_2": "100",
-        "answer_3": "10000",
-        "answer_4": "100000",
+        "question": "Which HTML attribute replaces an image, if not available?",
+        "answer_1": "longdesc",
+        "answer_2": "alt",
+        "answer_3": "title",
+        "answer_4": "src",
         "right_answer": 2
+    },
+    {
+        "question": "In HTML, which attribute is used to specify that an input field must be filled out?",
+        "answer_1": "required",
+        "answer_2": "validate",
+        "answer_3": "placeholder",
+        "answer_4": "formvalidate",
+        "right_answer": 1
     }
 
 ];
@@ -69,6 +69,8 @@ let questions = [
 
 let currentQuestion = 0;
 let correctAnswers = 0;
+let AUDIO_SUCCESS = new Audio('audio/win.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 function init() {
     document.getElementById('numberOfQuestions').innerHTML = questions.length;
@@ -78,49 +80,72 @@ function init() {
 
 function showQuestions() {
 
-    if (currentQuestion >= questions.length) {
-        document.getElementById('endScreen').style = '';
-        document.getElementById('questionBody').style = 'display: none;';
-        document.getElementById('correctAnswers').innerHTML = correctAnswers;
-        document.getElementById('allAnswers').innerHTML = questions.length;
+    if (gameIsOver()) {
+        showEndScreen();
     }
     else {
-        let percent = (currentQuestion + 1) / questions.length;
-        percent = Math.round(percent * 100);
-        console.log(percent);
-        document.getElementById('progressBar').innerHTML = `${percent}%`
-        document.getElementById('progressBar').style.width = `${percent}%`
-
-        let question = questions[currentQuestion];
-
-        document.getElementById('numberCurrentQuestion').innerHTML = currentQuestion + 1;
-
-        document.getElementById('questionText').innerHTML = question['question'];
-        document.getElementById('answer_1').innerHTML = question['answer_1'];
-        document.getElementById('answer_2').innerHTML = question['answer_2'];
-        document.getElementById('answer_3').innerHTML = question['answer_3'];
-        document.getElementById('answer_4').innerHTML = question['answer_4'];
+        updateProgressBar();
+        updateToNextQuestion();
     }
 }
+
+function gameIsOver(){
+    return currentQuestion >= questions.length;
+}
+
+function showEndScreen() {
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none;';
+    document.getElementById('correctAnswers').innerHTML = correctAnswers;
+    document.getElementById('allAnswers').innerHTML = questions.length;
+}
+
+function updateProgressBar(){
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progressBar').innerHTML = `${percent}%`
+    document.getElementById('progressBar').style.width = `${percent}%`
+}
+
+function updateToNextQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('numberCurrentQuestion').innerHTML = currentQuestion + 1;
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
 
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if (selectedQuestionNumber == question['right_answer']) {
+    if (rightAnswerSelected(selectedQuestionNumber, question)) {
         document.getElementById(selection).classList.add('bg-success');
+        AUDIO_SUCCESS.play();
         correctAnswers++;
+        
     }
     else {
         document.getElementById(selection).classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).classList.add('bg-success');
+        AUDIO_FAIL.play();
+        
     }
+    document.getElementById('disableClick').classList.add('disable-click');
     document.getElementById('next-button').disabled = false;
 }
 
+ function rightAnswerSelected(selectedQuestionNumber, question){
+    return selectedQuestionNumber == question['right_answer'];
+ }
+
 function nextQuestion() {
     currentQuestion++;
+    document.getElementById('disableClick').classList.remove('disable-click');
     document.getElementById('next-button').disabled = true;
     resetAnswerButtons();
     showQuestions();
